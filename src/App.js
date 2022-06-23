602d5812bcade5563e27033af09b6af6f68caa15
Marcus <marcusmoraes2010@hotmail.com>
2022-06-23 15:47:31 -0300

    feat: add header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Banner from "./components/Banner";
+import NavMenu from "./components/navMenu";
 import { MentoringGrouops } from "./pages";
 
 function App() {
   return (
     <BrowserRouter>
+      <Banner />
+      <NavMenu />
       <Routes>
         <Route path="/mentoring-groups" element={<MentoringGrouops />}></Route>
       </Routes>
