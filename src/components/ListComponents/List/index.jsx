0a7f2907de45d3f455dc3606f7f2c2288b08854b
Rodrigodcs <rodrigodcsee@gmail.com>
2022-09-09 09:37:59 -0300

    feat: date filter implemented

--- src/components/ListComponents/List/index.jsx
+++ src/components/ListComponents/List/index.jsx
@@ -2,8 +2,37 @@ import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import Applications from "../../../services/ApplicationsService";
 import fomatData from "../../../utils/fomatData";
+import { RangePicker } from "../../FormData/RangePicker";
 import Item from "../Item";
 import { ListContainer, Filter } from "./styles";
+
+const correct_date_filter = (date) => {
+  const day = date.day
+  const month = date.month
+  const year = date.year
+  return new Date(year,month-1,day)
+}
+
+const getFirstDayOfTheMonth = () => {
+    const date = new Date();
+    const newDate = new Date(date.getFullYear(), date.getMonth(), 1);
+    return {
+      day: newDate.getDate(),
+      month: newDate.getMonth() + 1,
+      year: newDate.getFullYear()
+    }
+  }
+
+  const getAtualDate = () => {
+    const date = new Date();
+    return {
+      day: date.getDate(),
+      month: date.getMonth() + 1,
+      year: date.getFullYear()
+    }
+  }
+
+  const initalRange = { from: null, to: null }
 
 const List = ({
   array,
@@ -14,10 +43,14 @@ const List = ({
   token,
 }) => {
   const [flag, setFlag] = useState(true);
-  //const [filteredApplications, setFilteredApplications] = useState(array) 
   const [company,setCompany] = useState("")
   const [job,setJob] = useState("Todos")
+  const [rangeSelected, setRangeSelected] = useState(initalRange);
 
+  console.log(rangeSelected)
+  console.log(rangeSelected.from===null? null:correct_date_filter(rangeSelected.from))
+  console.log(array)
+  console.log(rangeSelected.to===null? null:correct_date_filter(rangeSelected.to))
   const getAllApplications = () => {
     Applications.getAllApplications(token)
       .then(({ data }) => {
@@ -33,11 +66,23 @@ const List = ({
     getAllApplications();
   }, [flag]); //eslint-disable-line react-hooks/exhaustive-deps
   
+
+  function filterApplications(array){
+    return array.filter((app)=>{
+      return ((job==="Todos"?true:app.job===job) 
+      && app.company.includes(company) && 
+      (!rangeSelected.from?true:new Date(app.date)>=correct_date_filter(rangeSelected.from)) 
+      && (!rangeSelected.to?true:new Date(app.date)<=correct_date_filter(rangeSelected.to)))
+    })
+  }
+
+
   return (
     <ListContainer>
       <Filter>
-        <input onChange={(e)=> setCompany(e.target.value)}></input>
-        <select onChange={(e)=> setJob(e.target.value)}>
+        <p>Filtrar:</p>
+        <input className="option" placeholder="Empresa" onChange={(e)=> setCompany(e.target.value)}></input>
+        <select className="option" onChange={(e)=> setJob(e.target.value)}>
           <option value="Todos">Todos</option>
           <option value="Desenvolvedor FullStack">Desenvolvedor FullStack</option>
           <option value="Desenvolvedor Back-End">Desenvolvedor Back-End</option>
@@ -45,8 +90,13 @@ const List = ({
           <option value="Engenheiro de Software">Engenheiro de Software</option>
           <option value="Outros">Outros</option>
         </select>
+        <RangePicker
+            className="date"
+            onChange={(newRange) => setRangeSelected({ ...newRange })}
+            initialRange={initalRange}
+          />
       </Filter>
-      {array.filter((app)=>(job==="Todos"?true:app.job===job) && app.company.includes(company)).map((elem) => (
+      {filterApplications(array).map((elem) => (
         <Item
           key={elem.id}
           isWaiting={isWaiting}
@@ -64,5 +114,6 @@ const List = ({
     </ListContainer>
   );
 };
+
 
 export default List;
--- src/components/ListComponents/List/styles.js
+++ src/components/ListComponents/List/styles.js
@@ -7,8 +7,38 @@ const ListContainer = styled.ul`
 `;
 const Filter = styled.section`
   display: flex;
-  justify-content:space-between;
-  gap: 2rem;  
+  justify-content:flex-end;
+  flex-wrap: wrap;
+  align-items: center;
+  gap: 1rem;
+  max-width: 50%;
+  margin-left:50%;
+  p{
+    color:white;
+    font-size: 16px;
+  }
+  .option{
+    border-radius: 15px;
+    padding-left: 10px;
+    height: 30px;
+    min-width:150px;
+    display:flex;
+    flex: 1;
+    
+  }
+  .date{
+    border-radius: 15px;
+    padding-left: 10px;
+    height: 30px;
+    min-width:150px;
+    display:flex;
+    flex: 1;
+  }
+  /* @media (max-width: 875px) {
+    flex-wrap: wrap;
+    gap:2px;
+  } */
+  
 `;
 
 export { ListContainer, Filter };
