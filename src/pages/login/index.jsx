e854d6a6506b36c132768995720aafad1873dbb8
Guga <contato.gustavobarbosa@outlook.com>
2022-09-09 20:23:21 +0200

    legacy with google

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,37 +1,74 @@
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { Button, TextField } from "@mui/material";
+
+import { toast } from "react-toastify";
+
+import AuthContext from "../../contexts/AuthContext";
+import UserContext from "../../contexts/UserContext";
+import AuthService from "../../services/AuthServices";
+
+import { Container, AuthLabel, AuthWrapper, Box } from "./style";
+
 import LoginWithGoogle from "../../components/LoginWithGoogle";
 
-import { AuthLabel, AuthWrapper, Container } from "./style";
-
 export default function Login() {
-  return (
+  const [inputValue, setInputValue] = useState("");
+  const [isDisabled, setIsDisabled] = useState(true);
+  const { setToken } = useContext(AuthContext);
+  const { setIsMentor } = useContext(UserContext);
+  const navigate = useNavigate();
 
+  function submitInput(event) {
+    event.preventDefault();
+
+    AuthService.login(inputValue)
+      .then(({ data }) => {
+        setToken(data.token);
+        setIsMentor(data.is_mentor);
+        data.is_mentor ? navigate("/mentor") : navigate("/student");
+      })
+      .catch(({ response }) => {
+        if (response.status === 404) {
+          toast.error("Senha não encontrada!");
+        } else {
+          toast.error(
+            "Erro inesperado. Por favor, entre em contato com a coordenação!"
+          );
+        }
+        console.error(response);
+      });
+  }
+
+  return (
     <Container>
       <AuthWrapper>
         <AuthLabel>Entrar</AuthLabel>
         <Box data-cy="login-box" onSubmit={submitInput}>
-        <TextField
-          label="Senha de acesso"
-          variant="standard"
-          value={inputValue}
-          name="password"
-          onChange={(e) => {
-            setInputValue(e.target.value);
-            if (e.target.value.length === 36) setIsDisabled(false);
-            else !isDisabled && setIsDisabled(true);
-          }}
-          placeholder="XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"
-          inputProps={{ style: { textAlign: "center" } }}
-          sx={{ width: "80%" }}
-        />
-        <Button
-          variant="contained"
-          type="submit"
-          sx={{ fontWeight: 700, width: "30%" }}
-          disabled={isDisabled}
-        >
-          Entrar
-        </Button>
-      </Box>
+          <TextField
+            label="Senha de acesso"
+            variant="standard"
+            value={inputValue}
+            name="password"
+            onChange={(e) => {
+              setInputValue(e.target.value);
+              if (e.target.value.length === 36) setIsDisabled(false);
+              else !isDisabled && setIsDisabled(true);
+            }}
+            placeholder="XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"
+            inputProps={{ style: { textAlign: "center" } }}
+            sx={{ width: "80%" }}
+          />
+          <Button
+            variant="contained"
+            type="submit"
+            sx={{ fontWeight: 700, width: "30%" }}
+            disabled={isDisabled}
+          >
+            Entrar
+          </Button>
+        </Box>
         <LoginWithGoogle />
       </AuthWrapper>
     </Container>
--- src/pages/login/style.jsx
+++ src/pages/login/style.jsx
@@ -2,21 +2,33 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 100%;
-  height: calc(100vh - 6rem);
+  min-height: calc(100vh - 6rem);
+  padding: 0 64px;
 
   display: flex;
   justify-content: center;
   align-items: center;
 
   background-color: #000;
+
+  @media (max-width: 600px) {
+    padding: 32px 8px;
+  }
 `;
 
 export const AuthWrapper = styled.div`
+  width: 100%;
   padding: 24px;
   border-radius: 16px;
   border: 1px dashed var(--driven-color);
 
   position: relative;
+
+  display: flex;
+  gap: 32px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const AuthLabel = styled.div`
@@ -31,4 +43,24 @@ export const AuthLabel = styled.div`
   color: #fff;
 
   background-color: #000;
+`;
+
+export const Box = styled.form`
+  width: 50%;
+  max-width: 60rem;
+  height: 250px;
+  background-color: var(--darker);
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  border-radius: 1.5rem;
+
+  @media (max-width: 600px) {
+    width: 100%;
+    height: calc(100vh - 6rem);
+    justify-content: space-evenly;
+    border-radius: 0;
+  }
 `;
